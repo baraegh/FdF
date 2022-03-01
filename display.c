76c25b9dbf539c815291afe7a3eb4c913f9230b0
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-01 03:35:48 +0100

    fix bugs

--- display.c
+++ display.c
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 16:25:32 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/03/01 02:27:40 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "header.h"
 
-int get_diplay(t_data *data)
+int get_diplay(t_data *data, char* file_path)
 {
     data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		return (0);
-	data->win_ptr = mlx_new_window(data->mlx_ptr, W_WIDTH, W_HEIGHT, "");
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WIDTH, HEIGHT, file_path);
 	if (!data->win_ptr)
 	{
 		free(data->mlx_ptr);
 		return (0);
 	}
-	data->img.mlx_img = mlx_new_image(data->mlx_ptr, W_WIDTH, W_HEIGHT);
+	data->img.mlx_img = mlx_new_image(data->mlx_ptr, WIDTH, HEIGHT);
 	if (!data->img.mlx_img)
 	{
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
@@ -24,9 +35,9 @@ int get_diplay(t_data *data)
     return (1);
 }
 
-int	display(t_data data)
+int	display(t_data data, char* file_path)
 {
-	if (!get_diplay(&data))
+	if (!get_diplay(&data, file_path))
 	{
 		// mlx_destroy_image(data.mlx_ptr, &data.img);
 		// mlx_destroy_window(data.mlx_ptr, data.win_ptr);
