87ee9554d392cd4adf2087d31b0697c5314630a9
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-01 03:37:09 +0100

    fix Bresenham's algorithm

--- draw_line.c
+++ draw_line.c
@@ -1,11 +1,31 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_line.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27b_ 6:26:50 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/02/28 17:25:50 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "header.h"
+
+int	get_color(t_point b_p, t_point e_p)
+{
+	(void) e_p;
+
+	if (b_p.color_is_set == 0)
+		return (COLOR);
+	return (b_p.color);
+}
 
 t_var	set_values(t_data *data, t_point *b_p, t_point *e_p)
 {	
 	t_var var;
-
+	(void) data;
+	
 	var.dx = abs(e_p->x - b_p->x);
 	var.dy = -abs(e_p->y - b_p->y);
 	var.step_x = 1;
@@ -14,85 +34,36 @@ t_var	set_values(t_data *data, t_point *b_p, t_point *e_p)
 		var.step_x = -1;
 	if (b_p->y > e_p->y)
 		var.step_y = -1;
-	b_p->x *= data->zoom;
-	b_p->y *= data->zoom;
-	e_p->x *= data->zoom;
-	e_p->y *= data->zoom;
 	return (var);
-}
-
-int	get_color(t_point b_p, t_point e_p)
-{
-	(void) e_p;
-
-	if (b_p.z == 0)
-		return (COLOR);
-	return (b_p.color);
 }
 
 void	draw_line_bresenham(t_data *data, t_point b_p, t_point e_p)
 {
-	double	delta_x;
-	double	delta_y;
-	int		pixels;
-	double	pixel_x;
-	double	pixel_y;
-	int		i;
-	
-	b_p.x *= data->zoom;
-	b_p.y *= data->zoom;
-	e_p.x *= data->zoom;
-	e_p.y *= data->zoom;
-	isometric_proj(&b_p);
-	isometric_proj(&e_p);
+	t_var	var;
+	int		err;
+	int		err_i;
 
-	delta_x = e_p.x - b_p.x;
-	delta_y = e_p.y - b_p.y;
-	pixels = sqrt((delta_x * delta_x) + (delta_y * delta_y));
-	delta_x /= pixels;
-	delta_y /= pixels;
-	pixel_x = b_p.x;
-	pixel_y = b_p.y;
-	i = 0;
-	while (i < pixels)
+	var = set_values(data, &b_p, &e_p);
+	err = var.dx + var.dy;
+	while (1)
 	{
-		img_pix_put(&data->img, (t_point){pixel_x, pixel_y, 0, 0}, get_color(b_p, e_p));
-		pixel_x += delta_x;
-		pixel_y += delta_y;
-		i++;
+		// printf("color: %d\n", b_p.color);
+		img_pix_put(&data->img, b_p, b_p.color);
+		err_i = 2 * err;
+		if (err_i >= var.dy)
+		{
+			if (b_p.x == e_p.x)
+				break ;
+			err += var.dy;
+			b_p.x += var.step_x;
+		}
+		if (err_i <= var.dx)
+		{
+			if (b_p.y == e_p.y)
+				break ;
+			err += var.dx;
+			b_p.y += var.step_y;
+		}
 	}
 }
 
-// void	draw_line_bresenham(t_data *data, t_point b_p, t_point e_p)
-// {
-// 	t_var	var;
-// 	int		err;
-// 	// int		err_i;
-
-// 	var = set_values(data, &b_p, &e_p);
-// 	isometric_proj(&b_p);
-// 	isometric_proj(&e_p);
-// 	err = var.dx + var.dy;
-	// printf("err: %d, var.dx: %d, var.dy: %d\n-----------\n", err, var.dx, var.dy);
-	// while (1)
-	// {
-	// 	img_pix_put(&data->img, b_p, get_color(b_p, e_p));
-	// 	err_i = 2 * err;
-	// 	if (err_i >= var.dy)
-	// 	{
-	// 		printf("here y");
-	// 		if (b_p.x >= e_p.x) 
-	// 			break ;
-	// 		err += var.dy;
-	// 		b_p.x += var.step_x;
-	// 	}
-	// 	if (err_i >= var.dx)
-	// 	{
-	// 		printf("here x");
-	// 		if (b_p.y >= e_p.y)
-	// 			break ;
-	// 		err += var.dx;
-	// 		b_p.y += var.step_y;
-	// 	}
-	// }
-// }
