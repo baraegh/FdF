66ed137e485fbbdbb3b6266482cf2a5831514f37
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-02 23:12:43 +0100

    update the header.h

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 16:25:42 by eel-ghan          #+#    #+#             */
-/*   Updated: 2022/03/02 00:21:02 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2022/03/02 23:00:51 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "mlx.h"
 # include "includes/mlx_keys.h"
 # include "includes/struct.h"
+# include "includes/error.h"
 # include "get_next_line/get_next_line.h"
 # include "Libft/libft.h"
 # include <fcntl.h>
@@ -25,25 +26,25 @@
 # include <stdio.h>
 /*///////////////*/
 
-# define HEIGHT	1080
-# define WIDTH	1500
-# define DEFAULT_COLOR 0xB8405E
-# define Z_COLOR 0xEEE6CE
-# define ERROR -1
-#define TILE_Y 30
-# define TILE_X 30
+# define HEIGHT				1080
+# define WIDTH				1500
+# define BACKGROUND_COLOR	0x313552
+# define DEFAULT_COLOR		0xB8405E
+# define Z_COLOR			0xEEE6CE
 
 void	read_file(t_data *data, char *file_path);
 int		display(t_data data, char* file_path);
-int		render(t_data *data);
+void	render(t_data *data);
 void	img_pix_put(t_img *img, t_point p, int color);
 int		handle_no_event(void *data);
 int		handle_input(int keysym, t_data *data);
 int		handle_mouse(int x, int y, void *data);
 void	draw_line_bresenham(t_data *data, t_point b_p, t_point e_p);
-void	isometric_proj(t_point	*p, t_data *data);
 void	render_map(t_data *data);
 int		get_color(t_point b_p, t_point e_p);
-
+void	terminate(char *msg);
+void	free_arr(char **arr, char *msg);
+void	check_line(char *line, int fd);
+void	free_z_matrix(char ***matrix, char *line);
 
 #endif
