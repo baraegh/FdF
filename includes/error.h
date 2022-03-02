344137886b6f69d3e1c259b63fe5d9b1293c675c
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-02 23:13:28 +0100

    add error.h for error messages

---
+++ includes/error.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 02:09:42 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/03/02 23:05:31 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_H
+# define ERROR_H
+
+# define ERR_USAGE			"Usage: ./fdf MAP_FILE"
+# define ERR_MAP			"Incorrect MAP_FILE"
+# define ERR_MAP_READING	"Reading error, or file doesn't exist"
+# define ERR_SET_MATRIX		"Error while setting the matrix"
+# define ERR_DISPLAY		"Display error"
+# define ERR_RENDER			"Error while rendering"
+
+#endif
