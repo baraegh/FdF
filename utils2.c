cc73b7717018809afb516bda31b7055a39cd5e26
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-03 02:17:59 +0100

    add zoom function for zooming

---
+++ utils2.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 01:23:38 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/03/03 02:10:47 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+void	zoom(int key_code, t_data *data)
+{
+	if (key_code == KEY_PLUS)
+		data->zoom++;
+	else
+		data->zoom--;
+	if (data->zoom < 1)
+		data->zoom = 1;
+	render_map(data);
+}
